6dd4c3e1d5c1705b3ca0e10ee5eaaf7c9d14ca05
Damiano <damiano.massarelli@gmail.com>
2019-05-23 00:10:38 +0200

    add height map based terrain

---
+++ src/terrain/HeightMapTerrainHeightProvider.cpp
@@ -0,0 +1,44 @@
+#include "HeightMapTerrainHeightProvider.h"
+#include <stb_image.h>
+#include <iostream>
+#include <glm/glm.hpp>
+
+HeightMapTerrainHeightProvider::HeightMapTerrainHeightProvider(const std::string& heightMapPath, float minHeight, float maxHeight)
+ : mMinHeight{minHeight}, mMaxHeight{maxHeight}
+{
+    stbi_set_flip_vertically_on_load(false);
+    int cmp;
+    mHeightData = stbi_load(heightMapPath.c_str(), &mWidth, &mHeight, &cmp, STBI_grey);
+    if (mHeightData == nullptr)
+        std::cerr << "Cannot read height map " << heightMapPath << "\n";
+}
+
+float HeightMapTerrainHeightProvider::get(float x, float z) const
+{
+    if (mHeightData == nullptr)
+        return 0.0f;
+
+    // clamp
+    if (x < 0) x = 0;
+    if (x > 1) x = 1;
+
+    if (z < 0) z = 0;
+    if (z > 1) z = 1;
+
+    int zi = static_cast<int>(z * (mHeight - 1));
+    int xi = static_cast<int>(x * (mWidth - 1));
+
+    float f = (mHeightData[(zi * mWidth + xi)] / 255.0f);
+    return mMaxHeight * f + mMinHeight * (1.0f - f);
+}
+
+glm::vec3 HeightMapTerrainHeightProvider::getNormal(float x, float z) const
+{
+    float heightL = get(x - 2.0f / mWidth, z);
+    float heightR = get(x + 2.0f / mWidth, z);
+    float heightU = get(x, z + 2.0f / mHeight);
+    float heightD = get(x, z - 2.0f / mHeight);
+
+    return glm::normalize(glm::vec3{heightL - heightR, 2.0f, heightD - heightU});
+}
+
---
+++ src/terrain/TerrainHeightProvider.h
@@ -0,0 +1,17 @@
+#ifndef TERRAINHEIGHTPROVIDER_H
+#define TERRAINHEIGHTPROVIDER_H
+#include <glm/vec3.hpp>
+
+class TerrainHeightProvider
+{
+    public:
+        TerrainHeightProvider();
+
+        virtual float get(float x, float z) const = 0;
+
+        virtual glm::vec3 getNormal(float x, float z) const = 0;
+
+        virtual ~TerrainHeightProvider();
+};
+
+#endif // TERRAINHEIGHTPROVIDER_H
