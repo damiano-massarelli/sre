f31514defa37f6270a03ef7fea8f4ea370571ac4
Damiano <damiano.massarelli@gmail.com>
2019-08-03 11:58:42 +0200

    add debug utils

---
+++ src/debugUtils/debug.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <glad/glad.h>
+#include <iostream>
+
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar* message,
+	const void* userParam);
