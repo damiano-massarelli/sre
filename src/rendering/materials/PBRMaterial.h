27c1a03f109cea146339027d19a751ecf6fff95d
Alessio Paoletti <alessiopaoletti32@gmail.com>
2020-08-21 00:23:54 +0200

    Changed PBR default valures

--- src/rendering/materials/PBRMaterial.h
+++ src/rendering/materials/PBRMaterial.h
@@ -13,9 +13,9 @@ private:
 	Texture mAOMap;
 
 	glm::vec3 mAlbedo{ 1.f, 1.f, 1.f };
-	float mMetalness{ 1.f };
+	float mMetalness{ 0.f };
 	float mRoughness{ 1.f };
-	float mAO{ 1.f };
+	float mAO{ 0.f };
 
 	bool mUseAlbedoMap = true;
 	bool mUseNormalMap = true;
