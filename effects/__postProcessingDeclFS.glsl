7cc29a2b7fcbdec2a1a0f724763b89dd67dae4c6
Alessio Paoletti <alessiopaoletti32@gmail.com>
2020-12-30 22:51:54 +0100

    Added Camera uniform to the post processing FS declaration.

--- effects/__postProcessingDeclFS.glsl
+++ effects/__postProcessingDeclFS.glsl
@@ -5,3 +5,9 @@ out vec4 FragColor;
 uniform sampler2D screenTexture;
 uniform sampler2D depthTexture;
 
+layout (std140) uniform Camera {
+    vec3 cameraPosition;
+    float cameraNear;
+    vec3 cameraDirection;
+    float cameraFar;
+};
